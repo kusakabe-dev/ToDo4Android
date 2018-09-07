ef1fb16d9b129735c8949dd43c9069f3575eccab
syo-sa1982 <fujii1308@gmail.com>
2018-09-07 12:04:36 +0900

    feat:アプリケーションクラスの取得を簡略化

--- app/src/main/kotlin/com/syousa1982/todo4android/extension/FragmentExtension.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/extension/FragmentExtension.kt
@@ -1,4 +1,16 @@
 package com.syousa1982.todo4android.extension
 
+import android.support.v4.app.Fragment
+import com.syousa1982.todo4android.TodoApplication
+
+/**
+ * Fragment拡張
+ */
 object FragmentExtension
 
+/**
+ * アプリケーションクラスを取得
+ *
+ * @return TodoApplication
+ */
+fun Fragment.application(): TodoApplication = (activity?.application as TodoApplication)
