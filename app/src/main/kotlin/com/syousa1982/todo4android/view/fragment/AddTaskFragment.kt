4260fcdde59845c1ffe4f608ec6ccc3b304c481b
syo-sa1982 <fujii1308@gmail.com>
2018-09-17 11:31:39 +0900

    feat: タスク追加画面を作成

---
+++ app/src/main/kotlin/com/syousa1982/todo4android/view/fragment/AddTaskFragment.kt
@@ -0,0 +1,25 @@
+package com.syousa1982.todo4android.view.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.syousa1982.todo4android.databinding.FragmentAddTaskBinding
+
+
+/**
+ * タスク追加Fragment
+ *
+ */
+class AddTaskFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentAddTaskBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentAddTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
