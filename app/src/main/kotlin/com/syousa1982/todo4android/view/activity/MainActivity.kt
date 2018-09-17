47f09ac330e76ac89438ebfc334a4c8ccbb1236f
syo-sa1982 <fujii1308@gmail.com>
2018-09-17 23:56:37 +0900

    fix: 不要なオーバーライドを削除

--- app/src/main/kotlin/com/syousa1982/todo4android/view/activity/MainActivity.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/view/activity/MainActivity.kt
@@ -3,7 +3,6 @@ package com.syousa1982.todo4android.view.activity
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.os.Bundle
 import com.syousa1982.todo4android.R
 import com.syousa1982.todo4android.databinding.ActivityMainBinding
 import com.syousa1982.todo4android.extension.clear
@@ -19,10 +18,6 @@ class MainActivity : BaseActivity() {
      */
     private val binding by lazy {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateRootFragment(index: Int): BaseFragment {
