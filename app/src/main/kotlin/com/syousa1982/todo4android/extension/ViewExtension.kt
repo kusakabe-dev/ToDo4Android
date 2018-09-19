c393ea116ae95fd2cbe7056a2f77bb8f1f61c916
syo-sa1982 <fujii1308@gmail.com>
2018-09-20 08:33:23 +0900

    feat: プログレス表示用にViewを拡張

--- app/src/main/kotlin/com/syousa1982/todo4android/extension/ViewExtension.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/extension/ViewExtension.kt
@@ -1,5 +1,6 @@
 package com.syousa1982.todo4android.extension
 
+import android.databinding.BindingAdapter
 import android.os.Handler
 import android.view.View
 
@@ -7,7 +8,20 @@ import android.view.View
 /**
  * View 拡張オブジェクト
  */
-object ViewExtension
+object ViewExtension {
+
+    /**
+     * Viewの表示・非表示
+     *
+     * @param view View
+     * @param isVisible Boolean
+     */
+    @JvmStatic
+    @BindingAdapter("visibleOrGone")
+    fun setVisibleOrGone(view: View, isVisible: Boolean) {
+        view.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+}
 
 /**
  * 連打対策のためViewを一定時間クリック無効にする
