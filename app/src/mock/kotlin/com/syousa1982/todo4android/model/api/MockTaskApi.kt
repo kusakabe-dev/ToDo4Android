4c1c9eb32e7af23e346e2dfe6ee645e55f105228
syo-sa1982 <fujii1308@gmail.com>
2018-09-25 02:27:19 +0900

    test:モックAPIを実態と合わせる

--- app/src/mock/kotlin/com/syousa1982/todo4android/model/api/MockTaskApi.kt
+++ app/src/mock/kotlin/com/syousa1982/todo4android/model/api/MockTaskApi.kt
@@ -1,5 +1,9 @@
 package com.syousa1982.todo4android.model.api
 
+import com.syousa1982.todo4android.model.api.request.Request
+import com.syousa1982.todo4android.model.api.response.ItemResponse
+import com.syousa1982.todo4android.model.api.response.ItemsResponse
+import com.syousa1982.todo4android.model.api.response.MessageResponse
 import com.syousa1982.todo4android.model.entity.Task
 import io.reactivex.Single
 import retrofit2.mock.BehaviorDelegate
@@ -9,22 +13,33 @@ import retrofit2.mock.BehaviorDelegate
  */
 class MockTaskApi(private val delegate: BehaviorDelegate<TaskApi>) : TaskApi {
 
-    override fun fetchTasks(): Single<List<Task>> {
+    override fun fetchTasks(): Single<ItemsResponse<List<Task>>> {
         val tasks = listOf(
                 Task("1", "朝食を作る", true),
                 Task("2", "買い物", false),
                 Task("3", "通勤", false)
         )
-        return delegate.returningResponse(tasks).fetchTasks()
+        return delegate.returningResponse(ItemsResponse(tasks)).fetchTasks()
     }
 
-    override fun addTasks(task: Task): Single<MessageResponse> {
-        val response = MessageResponse(201)
-        return delegate.returningResponse(response).addTasks(task)
+    override fun fetchTask(id: String): Single<ItemResponse<Task>> {
+        val task = Task("3", "通勤", false)
+        return delegate.returningResponse(ItemsResponse(task)).fetchTask(id)
     }
 
-    override fun updateTasks(task: Task): Single<MessageResponse> {
-        val response = MessageResponse(204)
-        return delegate.returningResponse(response).updateTasks(task)
+    override fun addTasks(taskRequest: Request<Task>): Single<MessageResponse> {
+        val response = MessageResponse(true, "タスクを登録しました")
+        return delegate.returningResponse(response).addTasks(taskRequest)
     }
+
+    override fun updateTasks(id: String, taskRequest: Request<Task>): Single<MessageResponse> {
+        val response = MessageResponse(true, "タスクを更新しました")
+        return delegate.returningResponse(response).updateTasks(id, taskRequest)
+    }
+
+    override fun removeTasks(id: String): Single<MessageResponse> {
+        val response = MessageResponse(true, "タスクを削除しました")
+        return delegate.returningResponse(response).removeTasks(id)
+    }
+
 }
