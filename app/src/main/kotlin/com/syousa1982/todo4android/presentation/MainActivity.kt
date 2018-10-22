0ce129cbaaf25dcedd6f17f79f1ab0538e1078f3
syo-sa1982 <fujii1308@gmail.com>
2018-10-22 09:19:58 +0900

    いったんすべてやり直し

---
+++ app/src/main/kotlin/com/syousa1982/todo4android/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.syousa1982.todo4android.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.syousa1982.todo4android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
