6a64dafdfc543abaa42552c0f818989c573d66fa
fujiiyousuke <yosuke.fujii@nextinnovation-inc.co.jp>
2019-01-03 04:01:19 +0900

    fix:CIコカしてた&当分使う予定ないので一旦コメントアウト

--- app/src/main/kotlin/com/syousa1982/todo4android/data/api/TaskApi.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/data/api/TaskApi.kt
@@ -1,11 +1,9 @@
 package com.syousa1982.todo4android.data.api
 
-import com.syousa1982.todo4android.BuildConfig
 import com.syousa1982.todo4android.data.api.response.ItemsResponse
 import com.syousa1982.todo4android.data.api.response.TaskResponse
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Headers
 
 /**
  * タスクAPI
@@ -15,7 +13,7 @@ interface TaskApi {
     /**
      * タスクを取得
      */
-    @Headers("x-api-key: ${BuildConfig.OPEN_TASK_API_KEY}")
+//    @Headers("x-api-key: ${BuildConfig.OPEN_TASK_API_KEY}")
     @GET("tasks")
     fun fetchTasks(): Single<ItemsResponse<TaskResponse>>
 
