baff28b3916b30aa318a1713353107f85af96a56
fujiiyousuke <yosuke.fujii@nextinnovation-inc.co.jp>
2019-02-23 12:59:20 +0900

    fix:ライブラリのバージョン更新の影響でビルド落ちていたので切り戻し

--- app/src/main/kotlin/com/syousa1982/todo4android/presentation/top/TopFragment.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/presentation/top/TopFragment.kt
@@ -1,7 +1,6 @@
 package com.syousa1982.todo4android.presentation.top
 
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,25 +9,17 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.syousa1982.todo4android.R
 import com.syousa1982.todo4android.databinding.FragmentTopBinding
-import com.syousa1982.todo4android.presentation.BaseFragment
 import com.syousa1982.todo4android.util.extention.setOnClickPauseListener
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class TopFragment : BaseFragment() {
+class TopFragment : Fragment() {
 
-    lateinit var transitionFragmentListener: TransitionFragmentListener
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        transitionFragmentListener = requireActivity() as TransitionFragmentListener
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentTopBinding.inflate(inflater, container, false)
-        transitionFragmentListener.onFragmentTransaction(this)
         bindInput(binding)
         return binding.root
     }
