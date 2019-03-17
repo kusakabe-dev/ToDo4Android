f47c13ab63a051d47b5a9c1c0c7b8a4c6f2857bf
fujiiyousuke <fujii1308@gmail.com>
2019-03-17 19:12:57 +0900

    feat:ViewModelの作成とDI

--- app/src/main/kotlin/com/syousa1982/todo4android/App.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/App.kt
@@ -1,11 +1,14 @@
 package com.syousa1982.todo4android
 
 import android.app.Application
+import com.syousa1982.todo4android.di.AppModule
+import org.koin.android.ext.android.startKoin
 
 open class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin(this, listOf(AppModule.instance))
     }
 
     protected open fun isInUnitTests() = false
---
+++ app/src/main/kotlin/com/syousa1982/todo4android/presentation/tasklist/TaskListViewModel.kt
@@ -0,0 +1,10 @@
+package com.syousa1982.todo4android.presentation.tasklist
+
+import com.syousa1982.todo4android.presentation.BaseViewModel
+
+/**
+ * タスクリスト一覧 ViewModel
+ */
+class TaskListViewModel : BaseViewModel() {
+
+}
