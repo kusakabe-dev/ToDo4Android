29ad84eb9f9f3a168406d2430c4c642cd4349921
fujiiyousuke <fujii1308@gmail.com>
2019-03-27 08:38:02 +0900

    feat:viewModelにタスクリストを扱うLiveDataを配置

--- app/src/main/kotlin/com/syousa1982/todo4android/presentation/tasklist/TaskListViewModel.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/presentation/tasklist/TaskListViewModel.kt
@@ -1,10 +1,34 @@
 package com.syousa1982.todo4android.presentation.tasklist
 
+import androidx.lifecycle.MutableLiveData
+import com.syousa1982.todo4android.domain.Result
+import com.syousa1982.todo4android.domain.model.Task
+import com.syousa1982.todo4android.domain.model.TaskList
 import com.syousa1982.todo4android.presentation.BaseViewModel
 
 /**
  * タスクリスト一覧 ViewModel
+ * todo:usecaseをインジェクション
  */
 class TaskListViewModel : BaseViewModel() {
 
+    val taskList = MutableLiveData<Result<List<TaskList>>>()
+
+    override fun onResume() {
+        super.onResume()
+        taskList.value = Result.success(
+                listOf(
+                        TaskList(1, "ToDo", listOf(
+                                Task(1, "ほげ", Task.Status.DONE),
+                                Task(2, "会社を爆破", Task.Status.TODO),
+                                Task(3, "殺意駆動開発", Task.Status.TODO)
+                        )),
+                        TaskList(2, "家事", listOf(
+                                Task(1, "飯作る", Task.Status.DONE),
+                                Task(2, "皿洗う", Task.Status.DONE),
+                                Task(3, "掃除", Task.Status.TODO)
+                        ))
+                )
+        )
+    }
 }
