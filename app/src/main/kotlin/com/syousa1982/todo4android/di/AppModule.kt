a23c1eb9e1f56cc059aaf15dfa8730bac569c052
fujiiyousuke <fujii1308@gmail.com>
2019-03-30 12:44:42 +0900

    fix:幾つかのライブラリで依存関係が衝突しているらしいので、回避策を追加 - そのついでにKoinをAndroidX対応 - ビルドスクリプトも全体的に整理した

--- app/src/main/kotlin/com/syousa1982/todo4android/di/AppModule.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/di/AppModule.kt
@@ -2,7 +2,7 @@ package com.syousa1982.todo4android.di
 
 import com.syousa1982.todo4android.presentation.tasklist.TaskListViewModel
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 object AppModule {
