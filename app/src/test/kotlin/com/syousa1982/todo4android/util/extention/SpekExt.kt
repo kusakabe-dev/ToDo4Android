2467a490e755c2e35fbd3d9dd1264d6d685b4187
fujiiyousuke <fujii1308@gmail.com>
2019-04-14 18:06:21 +0900

    Test:LiveDataテスト用の拡張関数

---
+++ app/src/test/kotlin/com/syousa1982/todo4android/util/extention/SpekExt.kt
@@ -0,0 +1,26 @@
+package com.syousa1982.todo4android.util.extention
+
+import androidx.arch.core.executor.ArchTaskExecutor
+import androidx.arch.core.executor.TaskExecutor
+import org.jetbrains.spek.api.dsl.SpecBody
+
+fun SpecBody.useLiveData() {
+    beforeEachTest {
+        ArchTaskExecutor.getInstance().setDelegate(InstantTaskExecutor)
+    }
+    afterEachTest {
+        ArchTaskExecutor.getInstance().setDelegate(null)
+    }
+}
+
+private object InstantTaskExecutor : TaskExecutor() {
+    override fun executeOnDiskIO(runnable: Runnable) {
+        runnable.run()
+    }
+
+    override fun isMainThread(): Boolean = true
+
+    override fun postToMainThread(runnable: Runnable) {
+        runnable.run()
+    }
+}
