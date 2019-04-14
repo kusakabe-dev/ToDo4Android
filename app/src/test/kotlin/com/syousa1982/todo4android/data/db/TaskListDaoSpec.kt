fc4a4070525054d96b19294a6fad7c84013a1de6
fujiiyousuke <fujii1308@gmail.com>
2019-04-14 17:57:36 +0900

    Test:Daoのテストコードを整形

--- app/src/test/kotlin/com/syousa1982/todo4android/data/db/TaskListDaoSpec.kt
+++ app/src/test/kotlin/com/syousa1982/todo4android/data/db/TaskListDaoSpec.kt
@@ -11,7 +11,7 @@ import org.dbtools.android.room.jdbc.JdbcSQLiteOpenHelperFactory
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 
@@ -24,11 +24,11 @@ class TaskListDaoSpec : Spek({
 
         val database: AppDatabase by lazy {
             Room.inMemoryDatabaseBuilder(
-                    context,
-                    AppDatabase::class.java
+                context,
+                AppDatabase::class.java
             ).allowMainThreadQueries()
-                    .openHelperFactory(JdbcSQLiteOpenHelperFactory())
-                    .build()
+                .openHelperFactory(JdbcSQLiteOpenHelperFactory())
+                .build()
         }
         val result: Single<List<TaskListAndTasks>> by lazy {
             database.taskListDao().loadTaskListAndTasks()
@@ -36,19 +36,19 @@ class TaskListDaoSpec : Spek({
         context("Todoリストにタスク2件追加") {
             val taskList = TaskListEntity(1, "Todoリスト")
             val tasks = listOf(
-                    TaskEntity(1, 1, "hoge", "done"),
-                    TaskEntity(2, 1, "piyo", "todo")
+                TaskEntity(1, 1, "hoge", "done"),
+                TaskEntity(2, 1, "piyo", "todo")
             )
             database.taskListDao().insertTaskLists(taskList).test().await().assertComplete()
             tasks.forEach {
                 database.taskListDao().insertTasks(it).test().await().assertComplete()
             }
-            given("タスクリスト1件存在しているか") {
+            it("タスクリスト1件存在しているか") {
                 result.map {
                     it.count()
                 }.test().await().assertValue(1)
             }
-            given("タスク2件存在しているか") {
+            it("タスク2件存在しているか") {
                 result.map {
                     it.first().tasks.count()
                 }.test().await().assertValue(2)
