a8978e5d8595995275b3b8ff351fa73d96f35ce7
fujiiyousuke <fujii1308@gmail.com>
2019-04-16 09:15:45 +0900

    Test:期待値の定義を一箇所にまとめる

--- app/src/test/kotlin/com/syousa1982/todo4android/presentation/TaskListViewModelSpec.kt
+++ app/src/test/kotlin/com/syousa1982/todo4android/presentation/TaskListViewModelSpec.kt
@@ -22,6 +22,18 @@ class TaskListViewModelSpec : Spek({
         lateinit var observer: Observer<Result<List<TaskList>>>
         lateinit var todoUseCase: IToDoUseCase
         lateinit var viewModel: TaskListViewModel
+        val expectValue = Result.success(listOf(
+            TaskList(1, "todo", listOf(
+                Task(1, "aaaaa", Task.Status.DONE),
+                Task(2, "aaaaa", Task.Status.TODO),
+                Task(3, "aaaaa", Task.Status.TODO)
+            )),
+            TaskList(2, "家事", listOf(
+                Task(1, "飯作る", Task.Status.DONE),
+                Task(2, "皿洗う", Task.Status.DONE),
+                Task(3, "掃除", Task.Status.TODO)
+            ))
+        ))
         beforeEachTest {
             observer = mockk<Observer<Result<List<TaskList>>>>().also {
                 every { it.onChanged(any()) } just Runs
@@ -29,18 +41,7 @@ class TaskListViewModelSpec : Spek({
             todoUseCase = mockk<IToDoUseCase>().also {
                 every { it.getTaskLists() } answers {
                     Flowable.fromCallable {
-                        Result.success(listOf(
-                            TaskList(1, "todo", listOf(
-                                Task(1, "aaaaa", Task.Status.DONE),
-                                Task(2, "aaaaa", Task.Status.TODO),
-                                Task(3, "aaaaa", Task.Status.TODO)
-                            )),
-                            TaskList(2, "家事", listOf(
-                                Task(1, "飯作る", Task.Status.DONE),
-                                Task(2, "皿洗う", Task.Status.DONE),
-                                Task(3, "掃除", Task.Status.TODO)
-                            ))
-                        ))
+                        expectValue
                     }
                 }
             }
@@ -51,18 +52,6 @@ class TaskListViewModelSpec : Spek({
             run {
                 viewModel.fetchTasks()
             }
-            val expectValue = Result.success(listOf(
-                TaskList(1, "todo", listOf(
-                    Task(1, "aaaaa", Task.Status.DONE),
-                    Task(2, "aaaaa", Task.Status.TODO),
-                    Task(3, "aaaaa", Task.Status.TODO)
-                )),
-                TaskList(2, "家事", listOf(
-                    Task(1, "飯作る", Task.Status.DONE),
-                    Task(2, "皿洗う", Task.Status.DONE),
-                    Task(3, "掃除", Task.Status.TODO)
-                ))
-            ))
             assertEquals(expectValue, viewModel.taskLists.value)
         }
     }
