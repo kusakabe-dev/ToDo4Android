f05697e632522fa19356e1276e4ca61203010947
fujiiyousuke <fujii1308@gmail.com>
2019-05-04 01:36:24 +0900

    fix:不要なライブラリを削除

--- app/src/main/kotlin/com/syousa1982/todo4android/data/api/response/ItemsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.syousa1982.todo4android.data.api.response
-
-import com.squareup.moshi.Json
-import se.ansman.kotshi.JsonSerializable
-
-@JsonSerializable
-data class ItemsResponse<T>(
-        @Json(name = "Items") val items: List<T>
-)
