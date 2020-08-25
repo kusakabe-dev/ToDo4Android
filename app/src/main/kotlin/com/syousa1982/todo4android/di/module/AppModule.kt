df1750872047ff5bdc3f1f19d281dbad518820c0
fujiiyousuke <fujii1308@gmail.com>
2020-08-26 00:05:29 +0900

    リポジトリの依存注入

--- app/src/main/kotlin/com/syousa1982/todo4android/di/module/AppModule.kt
+++ app/src/main/kotlin/com/syousa1982/todo4android/di/module/AppModule.kt
@@ -2,6 +2,8 @@ package com.syousa1982.todo4android.di.module
 
 import android.app.Application
 import android.content.Context
+import com.syousa1982.todo4android.data.db.dao.TaskDao
+import com.syousa1982.todo4android.data.repository.TaskRepository
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -13,4 +15,8 @@ class AppModule {
     @Provides
     @Singleton
     fun provideContext(application: Application): Context = application
+
+    @Singleton
+    @Provides
+    fun provideTaskRepository(taskDao: TaskDao) = TaskRepository(taskDao)
 }
